67fa1940c9b834ff8bf69bcabb7d223d97492f55
alepaolazzi <alepaolazzidasilva@gmail.com>
2022-10-19 22:20:28 -0300

    adicionando curso sobre CRUD com JavaScript assíncrono

---
+++ front-end/crud-javascript-assincrono/admin/controller/cadastraClientes-controller.js
@@ -0,0 +1,16 @@
+import { clienteService } from "../service/cliente-service.js";
+
+const formulario = document.querySelector("[data-form]");
+
+formulario.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const nome = e.target.querySelector("[data-nome]").value;
+  const email = e.target.querySelector("[data-email]").value;
+  try {
+    await clienteService.criaCliente(nome, email);
+    window.location.href = "../telas/cadastro_concluido.html";
+  } catch (erro) {
+    console.log(erro);
+    window.location.href = "../telas/erro.html";
+  }
+});
